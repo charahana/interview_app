ee06948358719cdb400860b608f41a1e3300903a
charahana <charatwoha@outlook.jp>
2026-04-09 02:49:24 +0000

    feat: questionのCURD機能を追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,54 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destory]
+
   def index
     @questions = Question.all
   end
 
   def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path, notice: "作成しました"
+    else
+      flash.now[:alert] = "作成出来ませんでした"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to question_path(@question), notice: "更新しました"
+    else
+      flash.now[:alert] = "更新出来ませんでした"
+      render :edit
+    end
+  end
+  
+  def destory
+    if @question.destory
+      redirect_to questions_path, notice: "削除しました"
+    else
+      redirect_to questions_path, alert: "削除出来ませんでした"
+    end
+  end
+
+  private
+
+  def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:content, :answer, :category)
   end
 end
