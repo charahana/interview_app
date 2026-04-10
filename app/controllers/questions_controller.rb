2b2c355a123285c4b91aee468f23368db393b302
charahana <charatwoha@outlook.jp>
2026-04-10 02:22:33 +0000

    refactor: アットマーク付け忘れ

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = Question.new(question_params)
+    @question = Question.new(question_params)
     if @question.save
       redirect_to questions_path, notice: "作成しました"
     else
